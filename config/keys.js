b984fe4e84906b1e44c94c4ccab4aa86793d2a8d
Maryus Martsyalis <martsyalis@gmail.com>
2019-09-11 17:25:55 -0700

    passport Auth test

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: 'mongodb+srv://martsyalis:snova2Privet@cluster0-yoce4.mongodb.net/test?retryWrites=true&w=majority'
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+mongodb+srv://martsyalis:<password>@cluster0-yoce4.mongodb.net/test?retryWrites=true&w=majority
